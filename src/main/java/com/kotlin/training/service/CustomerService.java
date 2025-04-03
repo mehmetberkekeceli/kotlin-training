bfc1b20e146c87cc5a20be3a3eefa025e74eeeba
berkekeceli <berke.keceli@ikmbilisim.com.tr>
2025-04-03 16:38:47 +0300

    add: Services

---
+++ src/main/java/com/kotlin/training/service/CustomerService.java
@@ -0,0 +1,42 @@
+package com.kotlin.training.service;
+
+import com.kotlin.training.dto.CustomerDto;
+import com.kotlin.training.dto.converter.CustomerDtoConverter;
+import com.kotlin.training.exception.CustomerNotFoundException;
+import com.kotlin.training.model.Customer;
+import com.kotlin.training.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerDtoConverter converter;
+
+    public CustomerService(CustomerRepository customerRepository, CustomerDtoConverter converter) {
+        this.customerRepository = customerRepository;
+        this.converter = converter;
+    }
+
+    protected Customer findCustomerById(String id) {
+        return customerRepository.findById(id)
+                .orElseThrow(
+                        () -> new CustomerNotFoundException("Customer could not find by id: " + id));
+
+    }
+
+    public CustomerDto getCustomerById(String customerId) {
+        return converter.convertToCustomerDto(findCustomerById(customerId));
+    }
+
+    public List<CustomerDto> getAllCustomer() {
+
+        return customerRepository.findAll()
+                .stream()
+                .map(converter::convertToCustomerDto)
+                .collect(Collectors.toList());
+    }
+}
